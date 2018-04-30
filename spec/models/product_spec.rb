7691a740ce9351409278abc5f648aec191af381e
kulas115 <kulas115@gmail.com>
2018-04-30 07:33:12 +0200

    Specs for Product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:product) { FactoryBot.create(:product)}
+
+  it { is_expected.to validate_presence_of :title}
+  it { is_expected.to validate_uniqueness_of :title}
+  it { is_expected.to validate_presence_of :description}
+  it { is_expected.to validate_presence_of :image_url}
+  it { is_expected.to allow_value('test.jpg').for(:image_url)}
+  it { is_expected.to allow_value('test.png').for(:image_url)}
+  it { is_expected.to allow_value('test.gif').for(:image_url)}
+  it { is_expected.to_not allow_value('test.doc').for(:image_url)}
+  it { is_expected.to validate_numericality_of(:price).is_greater_than_or_equal_to(0.01) }
 end
