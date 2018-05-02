bff129682fd6df9c9c1e64de03291ee42b1a2d71
kulas115 <kulas115@gmail.com>
2018-05-02 09:11:48 +0200

    Update routes to include new scaffold-genarated files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root 'store#index', as: 'store_index'
 
+
+  resources :line_items
+  resources :carts
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'store#index'
 end
