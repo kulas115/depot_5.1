5419e259682f87f6adf1369e09493b5fdfb20950
kulas115 <kulas115@gmail.com>
2018-05-07 06:35:51 +0200

    Add atom feed to monitor order(s) status

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-
-
+  root to: 'store#index'
   resources :orders
   resources :line_items
   resources :carts
-  resources :products
+
+  resources :products do
+    get :who_bought, on: :member
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'store#index'
 end
