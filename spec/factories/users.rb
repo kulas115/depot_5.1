2a1aefb38c92ca2ab1b72dd03f51f058339270f2
kulas115 <kulas115@gmail.com>
2018-05-16 07:23:28 +0200

    Change new user page layout, update model and specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :user do
-    name "MyString"
-    password ""
+    name "Norville"
+    password "foobar"
+    password_confirmation "foobar"
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -7,4 +7,24 @@ RSpec.describe "Users", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "POST /users" do
+    it "should create user" do
+      expect {
+        post "/users", params: { user: { name: 'sam', password: 'foobar', password_confirmation: 'foobar'}}
+      }.to change(User, :count).by(1)
+
+      expect(response).to redirect_to users_path
+    end
+  end
+
+  describe "PATCH /user" do
+    it "should update user" do
+      user = FactoryBot.create(:user)
+
+      patch "/users/#{user.id}", params: { user: { name: 'sam', password: 'foobar', password_confirmation: 'foobar'}}
+
+      expect(response).to redirect_to users_path
+    end
+  end
 end
