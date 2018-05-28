7d624c9a7b82f9b312e0cfa8f8065d344db58905
kulas115 <kulas115@gmail.com>
2018-05-28 08:15:38 +0200

    Modify routes to include locale at the beggining of path, add before_action to check to locale, initialize I18n

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,14 @@ Rails.application.routes.draw do
   end
 
   resources :users
-  root to: 'store#index'
-  resources :orders
-  resources :line_items
-  resources :carts
-
   resources :products do
     get :who_bought, on: :member
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root to: 'store#index', via: :all
+  end
 end
